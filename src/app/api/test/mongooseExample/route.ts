2e96b12020317e1d306381d0368bfb87605f7df8
cechecco <desalvadorfrancesco@gmail.com>
2023-05-16 11:30:54 +0200

    ADD mongoose

--- src/app/api/test/mongooseExample/route.ts
+++ src/app/api/test/mongooseExample/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   try {
     await connectMongo();
+    
     const test = await Test.create(await req.json());
 
     return NextResponse.json({ test });
