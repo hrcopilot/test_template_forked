85d844974e604a35ccc831df9f4143ef6156dd2d
cechecco <desalvadorfrancesco@gmail.com>
2023-05-16 11:28:25 +0200

    ,

---
+++ src/app/api/test/mongooseExample/route.ts
@@ -0,0 +1,15 @@
+import Test from '@/lib/test.util.template.model';
+import connectMongo from '../../../../lib/util.template.mongoose';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectMongo();
+    const test = await Test.create(await req.json());
+
+    return NextResponse.json({ test });
+  } catch (error) {
+    console.log('--------', error);
+    return NextResponse.json({ error });
+  }
+}
